e0d811e27559d0535155069b2b13139ad672ad1e
jedtrow <jtrow@thetechbit.com.au>
2017-10-13 10:48:50 +1100

    Bug fixes and improvements
    Fixed issue: Does not work for graphs with negative values #1
    Added cornerRadius to be configurable from chart options

--- Chart.roundedBarCharts.js
+++ Chart.roundedBarCharts.js
@@ -1,16 +1,18 @@
 /*
 *   Rounded Rectangle Extension for Bar Charts and Horizontal Bar Charts
-*   Tested with Charts.js 2.5.0
+*   Tested with Charts.js 2.7.0
 */
 Chart.elements.Rectangle.prototype.draw = function() {
-    
+
     var ctx = this._chart.ctx;
     var vm = this._view;
     var left, right, top, bottom, signX, signY, borderSkipped, radius;
     var borderWidth = vm.borderWidth;
-    // Set Radius Here
-    // If radius is large enough to cause drawing errors a max radius is imposed
-    var cornerRadius = 5;
+
+    // If radius is less than 0 or is large enough to cause drawing errors a max
+    //      radius is imposed
+    var cornerRadius = this._chart.config.options.cornerRadius;
+    if(cornerRadius < 0){ cornerRadius = 0; }
 
     if (!vm.horizontal) {
         // bar
@@ -99,30 +101,70 @@ Chart.elements.Rectangle.prototype.draw = function() {
         height = corners[0][1] - corners[1][1];
         x = corners[1][0];
         y = corners[1][1];
-        
+
         var radius = cornerRadius;
-        
         // Fix radius being too large
-        if(radius > height/2){
-            radius = height/2;
-        }if(radius > width/2){
-            radius = width/2;
+        if(radius > Math.abs(height)/2){
+            radius = Math.floor(Math.abs(height)/2);
+        }
+        if(radius > Math.abs(width)/2){
+            radius = Math.floor(Math.abs(width)/2);
         }
 
-        ctx.moveTo(x + radius, y);
-        ctx.lineTo(x + width - radius, y);
-        ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
-        ctx.lineTo(x + width, y + height - radius);
-        ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
-        ctx.lineTo(x + radius, y + height);
-        ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
-        ctx.lineTo(x, y + radius);
-        ctx.quadraticCurveTo(x, y, x + radius, y);
+        if(height < 0){
+            // Negative values in a standard bar chart
+            x_tl = x;           x_tr = x+width;
+            y_tl = y+height;    y_tr = y+height;
 
+            x_bl = x;           x_br = x+width;
+            y_bl = y;           y_br = y;
+
+            // Draw
+            ctx.moveTo(x_bl+radius, y_bl);
+            ctx.lineTo(x_br-radius, y_br);
+            ctx.quadraticCurveTo(x_br, y_br, x_br, y_br-radius);
+            ctx.lineTo(x_tr, y_tr+radius);
+            ctx.quadraticCurveTo(x_tr, y_tr, x_tr-radius, y_tr);
+            ctx.lineTo(x_tl+radius, y_tl);
+            ctx.quadraticCurveTo(x_tl, y_tl, x_tl, y_tl+radius);
+            ctx.lineTo(x_bl, y_bl-radius);
+            ctx.quadraticCurveTo(x_bl, y_bl, x_bl+radius, y_bl);
+
+        }else if(width < 0){
+            // Negative values in a horizontal bar chart
+            x_tl = x+width;     x_tr = x;
+            y_tl= y;            y_tr = y;
+
+            x_bl = x+width;     x_br = x;
+            y_bl = y+height;    y_br = y+height;
+
+            // Draw
+            ctx.moveTo(x_bl+radius, y_bl);
+            ctx.lineTo(x_br-radius, y_br);
+            ctx.quadraticCurveTo(x_br, y_br, x_br, y_br-radius);
+            ctx.lineTo(x_tr, y_tr+radius);
+            ctx.quadraticCurveTo(x_tr, y_tr, x_tr-radius, y_tr);
+            ctx.lineTo(x_tl+radius, y_tl);
+            ctx.quadraticCurveTo(x_tl, y_tl, x_tl, y_tl+radius);
+            ctx.lineTo(x_bl, y_bl-radius);
+            ctx.quadraticCurveTo(x_bl, y_bl, x_bl+radius, y_bl);
+
+        }else{
+            //Positive Value
+            ctx.moveTo(x + radius, y);
+            ctx.lineTo(x + width - radius, y);
+            ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+            ctx.lineTo(x + width, y + height - radius);
+            ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+            ctx.lineTo(x + radius, y + height);
+            ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+            ctx.lineTo(x, y + radius);
+            ctx.quadraticCurveTo(x, y, x + radius, y);
+        }
     }
 
     ctx.fill();
     if (borderWidth) {
         ctx.stroke();
     }
-}; 
+};
--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # Chart.js-Rounded-Bar-Charts
 Rounded Rectangles in Bar Charts
 
-Tested with Chart.js 2.5.0
+Tested with Chart.js 2.7.0
 
-To use include as so:
+To use include the following:
+```html
+<script type="text/javascript" charset="utf-8" src="js/Chart.min.js"></script>
+<script type="text/javascript" charset="utf-8" src="js/Chart.roundedBarCharts.min.js"></script>
+```
 
-'<script type="text/javascript" charset="utf-8" src="js/Chart.min.js"></script>
-<script type="text/javascript" charset="utf-8" src="js/Chart.extentions.js"></script>'
+Then set the radius in the options e.g.
+```javascript
+var options = {
+    cornerRadius: 20,
+};
+```
